d88943b0a348f92ee34c07e299f48e7e1d1565ae
sunmengyuan <1099048846@qq.com>
2021-09-17 15:12:52 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,7 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
+$.ajaxPrefilter(function (options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   options.url = 'http://ajax.frontend.itheima.net' + options.url
 
@@ -12,8 +12,9 @@ $.ajaxPrefilter(function(options) {
     }
   }
 
+  // 为了限制用户访问权限，不论成功还是失败，最终都会调用 complete 回调函数
   // 全局统一挂载 complete 回调函数
-  options.complete = function(res) {
+  options.complete = function (res) {
     // console.log('执行了 complete 回调：')
     // console.log(res)
     // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
